8df885d8aa86144a6db8e5ae6a3abf8ec5a76660
Dominic Betts <dobett@microsoft.com>
2018-05-03 09:49:20 +0100

    Add license headers

--- Quickstarts/simulated-device/SimulatedDevice.js
+++ Quickstarts/simulated-device/SimulatedDevice.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 'use strict';
 
 // The device connection string to authenticate the device with your IoT hub.
