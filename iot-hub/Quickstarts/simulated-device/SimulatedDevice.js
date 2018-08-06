a82cb2a51f6925b2fa87275218b01678c5aadfe1
Pierre Cauchois <pierreca@microsoft.com>
2018-08-06 16:00:15 -0700

    Simplify simulated device samples

--- iot-hub/Quickstarts/simulated-device/SimulatedDevice.js
+++ iot-hub/Quickstarts/simulated-device/SimulatedDevice.js
@@ -25,29 +25,27 @@ var Message = require('azure-iot-device').Message;
 
 var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
 
-// Print results.
-function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) console.log(op + ' error: ' + err.toString());
-    if (res) console.log(op + ' status: ' + res.constructor.name);
-  };
-}
-
 // Create a message and send it to the IoT hub every second
 setInterval(function(){
   // Simulate telemetry.
   var temperature = 20 + (Math.random() * 15);
-  var humidity = 60 + (Math.random() * 20);
-
-  // Add the telemetry to the message body.
-  var data = JSON.stringify({ temperature: temperature, humidity: humidity });
-  var message = new Message(data);
+  var message = new Message(JSON.stringify({
+    temperature: temperature,
+    humidity: 60 + (Math.random() * 20)
+  }));
 
   // Add a custom application property to the message.
   // An IoT hub can filter on these properties without access to the message body.
   message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+
   console.log('Sending message: ' + message.getData());
 
   // Send the message.
-  client.sendEvent(message, printResultFor('send'));
+  client.sendEvent(message, function (err) {
+    if (err) {
+      console.error('send error: ' + err.toString());
+    } else {
+      console.log('message sent');
+    }
+  });
 }, 1000);
