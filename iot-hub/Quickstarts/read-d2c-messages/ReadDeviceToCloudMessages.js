6ea7a494c9e87d80f3485fd11bd5cf401fc118f4
Ramya Achutha Rao <ramyar@microsoft.com>
2020-04-27 00:33:57 -0700

    Update sample to use latest EH package

--- iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
+++ iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
@@ -1,26 +1,25 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT Licence.
 
-'use strict';
+/*
+  This sample demonstrates how to use the Microsoft Azure Event Hubs Client for JavaScript to 
+  read messages sent from a device. Please see the documentation for @azure/event-hubs package
+  for more details at https://www.npmjs.com/package/@azure/event-hubs
 
-// Connection string for the IoT Hub service
-//
-// NOTE:
-// For simplicity, this sample sets the connection string in code.
-// In a production environment, the recommended approach is to use
-// an environment variable to make it available to your application
-// or use an x509 certificate.
-// https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
-//
-// Using the Azure CLI:
-// az iot hub show-connection-string --hub-name {YourIoTHubName} --policy-name service  --output table
-var connectionString = '{Your service connection string here}';
+  For an example that uses checkpointing, follow up this sample with the sample in the 
+  eventhubs-checkpointstore-blob package on GitHub at the following link:
 
-// Using the Node.js SDK for Azure Event hubs:
-//   https://github.com/Azure/azure-event-hubs-node
-// The sample connects to an IoT hub's Event Hubs-compatible endpoint
-// to read messages sent from a device.
-var { EventHubClient, EventPosition } = require('@azure/event-hubs');
+  https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/eventhub/eventhubs-checkpointstore-blob/samples/javascript/receiveEventsUsingCheckpointStore.js
+*/
+
+const { EventHubConsumerClient } = require("@azure/event-hubs");
+
+// If using websockets, uncomment the below require statement
+// const WebSocket = require("ws");
+
+// If you need proxy support, uncomment the below code to create proxy agent
+// const HttpsProxyAgent = require("https-proxy-agent");
+// const proxyAgent = new HttpsProxyAgent(proxyInfo);
 
 var printError = function (err) {
   console.log(err.message);
@@ -28,28 +27,59 @@ var printError = function (err) {
 
 // Display the message content - telemetry and properties.
 // - Telemetry is sent in the message body
-// - The device can add arbitrary application properties to the message
+// - The device can add arbitrary properties to the message
 // - IoT Hub adds system properties, such as Device Id, to the message.
-var printMessage = function (message) {
-  console.log('Telemetry received: ');
-  console.log(JSON.stringify(message.body));
-  console.log('Application properties (set by device): ')
-  console.log(JSON.stringify(message.applicationProperties));
-  console.log('System properties (set by IoT Hub): ')
-  console.log(JSON.stringify(message.annotations));
-  console.log('');
+var printMessages = function (messages) {
+  for (const message of messages) {
+    console.log("Telemetry received: ");
+    console.log(JSON.stringify(message.body));
+    console.log("Properties (set by device): ");
+    console.log(JSON.stringify(message.properties));
+    console.log("System properties (set by IoT Hub): ");
+    console.log(JSON.stringify(message.systemProperties));
+    console.log("");
+  }
 };
 
-// Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
-// This example only reads messages sent after this application started.
-var ehClient;
-EventHubClient.createFromIotHubConnectionString(connectionString).then(function (client) {
-  console.log("Successfully created the EventHub Client from iothub connection string.");
-  ehClient = client;
-  return ehClient.getPartitionIds();
-}).then(function (ids) {
-  console.log("The partition ids are: ", ids);
-  return ids.map(function (id) {
-    return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
+async function main() {
+  console.log("IoT Hub Quickstarts - Read device to cloud messages.");
+
+  // Event Hub-compatible endpoint
+  // az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+  const s_eventHubsCompatibleEndpoint = "{your Event Hubs compatible endpoint}";
+
+  // Event Hub-compatible name
+  // az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+  const s_eventHubsCompatiblePath = "{your Event Hubs compatible name}";
+
+  // Primary key for the "service" policy to read messages
+  // az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+  const s_iotHubSasKey = "{your service primary key}";
+
+  const connectionString = `Endpoint=${s_eventHubsCompatibleEndpoint}/;EntityPath=${s_eventHubsCompatiblePath};SharedAccessKeyName=service;SharedAccessKey=${s_iotHubSasKey}`;
+
+  // If using websockets, uncomment the webSocketOptions below
+  // If using proxy, then set `webSocketConstructorOptions` to
+  // { agent: proxyAgent }
+  // You can also use the `retryOptions` in the client options to configure the retry policy
+  const clientOptions = {
+    // webSocketOptions: {
+    //   webSocket: WebSocket,
+    //   webSocketConstructorOptions: {}
+    // }
+  };
+
+  // Create the client to connect to the default consumer group of the Event Hub
+  const consumerClient = new EventHubConsumerClient("$Default", connectionString, clientOptions);
+
+  // Subscribe to messages from all partitions as below
+  // To subscribe to messages from a single partition, use the overload of the same method.
+  consumerClient.subscribe({
+    processEvents: printMessages,
+    processError: printError,
   });
-}).catch(printError);
+}
+
+main().catch((error) => {
+  console.error("Error running sample:", error);
+});
--- iot-hub/Quickstarts/read-d2c-messages/readme.md
+++ iot-hub/Quickstarts/read-d2c-messages/readme.md
@@ -1,15 +1,43 @@
 # Notes
 
-## Pre-requisite ##
-- **Node.js version: 6.x or higher.** 
-- We would **still encourage you** to install the latest available LTS version at any given time from https://nodejs.org. **It is a good practice to always install the latest available LTS version of node.js.**
-- Installing node.js on **Windows or macOS** is very simple with available installers on the [node.js website](https://nodejs.org). If you are using a **linux based OS**, then you can find easy to follow, one step installation instructions over [here](https://nodejs.org/en/download/package-manager/).
+This sample demonstrates how to use the Microsoft Azure Event Hubs Client for JavaScript to 
+read messages sent from a device by using the built-in event hubs that exists by default for
+every Iot Hub instance. 
 
-This example uses the [Node.js SDK for Azure Event hubs](https://github.com/Azure/azure-event-hubs-node) to read from your IoT hub's Event Hub-compatible endpoint.
+## Get Event Hubs-compatible connection string
 
-Typically, you connect another service to this endpoint to process and deliver the telemetry to its ultimate destination. For example, you can connect the following services to this Events endpoint on IoT Hub:
+You can get the Event Hubs-compatible connection string to your IotHub instance via the Azure portal or
+by using the Azure CLI.
 
-- Azure Stream Analytics
-- Logic Apps
-- Azure Databricks
-- Spark on HDInsight
+If using the Azure portal, see [Built in endpoints for IotHub](https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-messages-read-builtin#read-from-the-built-in-endpoint)
+
+If using the Azure CLI, you will need to run the below before running this sample to get 
+the details required to form the Event Hubs compatible connection string
+
+- `az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}`
+- `az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}`
+- `az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}`
+
+## Checkpointing
+
+For an example that uses checkpointing, follow up this sample with the [sample that uses
+Azure Storage Blob to create checkpoints](https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/eventhub/eventhubs-checkpointstore-blob/samples/javascript/receiveEventsUsingCheckpointStore.js).
+
+## WebSocket and proxy
+
+If using WebSockets, then you need to bring in your own WebSocket implementation.
+
+If your application runs in the browser, then you can get the WebSocket from `window.WebSocket`.
+Otherwise, you can get an implementation from any of the popular packages like [ws](https://www.npmjs.com/package/ws).
+
+If your application runs behind a proxy server, then you first need to create an agent with the relevant information.
+```javascript
+const HttpsProxyAgent = require("https-proxy-agent");
+const proxyAgent = new HttpsProxyAgent(proxyInfo);
+```
+
+You can then pass the agent to the websocket options as shown in this sample.
+
+
+
+
